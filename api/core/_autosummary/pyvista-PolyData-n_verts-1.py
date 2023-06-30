e7b2e4683cefbd03b9fac3bad20c084a5507956f
pyvista/pyvista-docs-dev-ja <pyvista/pyvista-docs-dev-ja>
2023-06-30 04:13:04 +0000

    [ci skip] 14674475496 https://github.com/pyvista/pyvista-docs-dev-ja/actions/runs/5418631131/jobs/9850911516

--- api/core/_autosummary/pyvista-PolyData-n_verts-1.py
+++ api/core/_autosummary/pyvista-PolyData-n_verts-1.py
@@ -1,8 +1,19 @@
 # Create a simple mesh containing just two points and return the
-# number of vertices.
+# number of vertices. By default, when constructing a PolyData with points but no cells,
+# vertices are automatically created, one per point.
 #
 import pyvista
 mesh = pyvista.PolyData([[1.0, 0.0, 0.0], [1.0, 1.0, 1.0]])
-mesh.n_verts
+mesh.n_points, mesh.n_verts
 # Expected:
-## 2
+## (2, 2)
+#
+# If any other cells are specified, these vertices are not created.
+#
+import pyvista
+mesh = pyvista.PolyData(
+    [[1.0, 0.0, 0.0], [1.0, 1.0, 1.0]], lines=[2, 0, 1]
+)
+mesh.n_points, mesh.n_verts
+# Expected:
+## (2, 0)
--- api/plotting/_autosummary/pyvista-Plotter-export_html-1.py
+++ api/plotting/_autosummary/pyvista-Plotter-export_html-1.py
@@ -1,5 +1,3 @@
-# Export as a three.js scene using the pythreejs backend.
-#
 import pyvista
 from pyvista import examples
 mesh = examples.load_uniform()
@@ -12,9 +10,3 @@ _ = pl.add_mesh(
     mesh, scalars='Spatial Cell Data', show_edges=True
 )
 pl.export_html('pyvista.html')  # doctest:+SKIP
-#
-# Export as a vtk.js scene using the panel backend.
-#
-pl.export_html(
-    'pyvista_panel.html', backend='panel'
-)  # doctest:+SKIP
