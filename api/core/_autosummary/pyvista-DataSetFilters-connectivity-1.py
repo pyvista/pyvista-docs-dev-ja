59414c038c4193a3820e1fb06ff1f8a0700e02b6
pyvista/pyvista-docs-dev-ja <pyvista/pyvista-docs-dev-ja>
2023-09-15 06:25:51 +0000

    [ci skip] 16813730036 https://github.com/pyvista/pyvista-docs-dev-ja/actions/runs/6192940105/job/16813730036

--- api/core/_autosummary/pyvista-DataSetFilters-connectivity-1.py
+++ api/core/_autosummary/pyvista-DataSetFilters-connectivity-1.py
@@ -1,122 +1,50 @@
-# Label all connected regions.
+# Create a single mesh with three disconnected regions where each
+# region has a different cell count.
 #
-# Load data.
-#
-import numpy as np
-from pyvista import examples
-import pyvista as pv
-mesh = examples.download_foot_bones()
-#
-# Extract all disconnected regions.
-#
-conn = mesh.connectivity()
-#
-# Configure plotting parameters.
-#
-categories = True
-cmap = 'glasbey'
-#
-# Format scalar bar text for integer values.
-#
-scalar_bar_args = dict(
-    fmt='%.f',  # Do not show decimals
+import pyvista
+large = pyvista.Sphere(
+    center=(-4, 0, 0), phi_resolution=40, theta_resolution=40
 )
-#
-# Set the plot's camera position.
-#
-cpos = [(10.5, 12.2, 18.3), (0.0, 0.0, 0.0), (0.0, 1.0, 0.0)]
-#
-# Plot the regions by ID.
-#
-# Note that region IDs are sorted in descending order by
-# cell count.
-#
-conn.plot(
-    categories=categories,
-    cmap=cmap,
-    scalar_bar_args=scalar_bar_args,
-    cpos=cpos,
+medium = pyvista.Sphere(
+    center=(-2, 0, 0), phi_resolution=15, theta_resolution=15
 )
-#
-# Extract specific regions by size.
-# Calculate region sizes.
-#
-# Get counts using the previous `connectivity('all')` results.
-#
-regions, region_sizes = np.unique(
-    conn['RegionId'], return_counts=True
+small = pyvista.Sphere(
+    center=(0, 0, 0), phi_resolution=7, theta_resolution=7
 )
+mesh = large + medium + small
 #
-# Extract regions with fewer than 150 cells.
+# Plot their connectivity.
 #
-region_ids = regions[np.flatnonzero(region_sizes < 150)]
-conn = mesh.connectivity('specified', region_ids)
+conn = mesh.connectivity('all')
+conn.plot(cmap=['red', 'green', 'blue'], show_edges=True)
 #
-# Plot result with plotting parameters defined earlier.
+# Restrict connectivity to a scalar range.
 #
-conn.plot(
-    categories=categories,
-    cmap=cmap,
-    scalar_bar_args=scalar_bar_args,
-    cpos=cpos,
-)
+mesh['y_coordinates'] = mesh.points[:, 1]
+conn = mesh.connectivity('all', scalar_range=[-1, 0])
+conn.plot(cmap=['red', 'green', 'blue'], show_edges=True)
+#
+# Extract the region closest to the origin.
+#
+conn = mesh.connectivity('closest', (0, 0, 0))
+conn.plot(color='blue', show_edges=True)
+#
+# Extract a region using a cell ID ``100`` as a seed.
+#
+conn = mesh.connectivity('cell_seed', 100)
+conn.plot(color='green', show_edges=True)
 #
 # Extract the largest region.
 #
-conn = mesh.connectivity('largest', label_regions=False)
+conn = mesh.connectivity('largest')
+conn.plot(color='red', show_edges=True)
 #
-# Plot the largest region and show the input mesh for reference.
+# Extract the largest and smallest regions by specifying their
+# region IDs. Note that the region IDs of the output differ from
+# the specified IDs since the input has three regions but the output
+# only has two.
 #
-p = pv.Plotter()
-_ = p.add_mesh(conn)
-_ = p.add_mesh(mesh, style='wireframe')
-p.show(cpos=cpos)
-#
-# Extract regions using seed points.
-#
-# Create hills and use curvature to define their peaks and valleys.
-#
-import pyvista as pv
-mesh = pv.ParametricRandomHills()
-mesh["Curvature"] = mesh.curvature()
-#
-# Visualize the peaks and valleys.
-#
-# Peaks have large positive curvature (i.e. are convex)
-# Valleys have large negative curvature (i.e. are concave)
-# Flat regions have curvature close to zero.
-#
-mesh.plot(
-    clim=[-0.5, 0.5],
-    cmap='bwr',
-    below_color='blue',
-    above_color='red',
-)
-#
-# Extract the steepest peak using a seed point.
-#
-data_min, data_max = mesh.get_data_range()
-peak_range = [0.2, data_max]  # Peak if curvature > 0.2
-peak_point_id = np.argmax(mesh['Curvature'])  # Steepest point
-peak_mesh = mesh.connectivity(
-    'point_seed', peak_point_id, scalar_range=peak_range
-)
-#
-# Extract the valley region closest to the steepest peak.
-#
-valley_range = [data_min, -0.2]  # Valley if curvature < 0.2
-peak_point = mesh.points[peak_point_id]
-valley_mesh = mesh.connectivity(
-    'closest', peak_point, scalar_range=valley_range
-)
-#
-# Plot extracted regions.
-#
-p = pv.Plotter()
-_ = p.add_mesh(mesh, style='wireframe', color='lightgray')
-_ = p.add_mesh(peak_mesh, color='red', label='Steepest Peak')
-_ = p.add_mesh(
-    valley_mesh, color='blue', label='Closest Valley'
-)
-_ = p.add_legend()
-p.show()
+large_id = 0  # largest always has ID '0'
+small_id = 2  # smallest has ID 'N-1' with N=3 regions
+conn = mesh.connectivity('specified', (small_id, large_id))
+conn.plot(cmap=['red', 'blue'], show_edges=True)
