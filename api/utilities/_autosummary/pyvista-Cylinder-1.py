de2eb7b8f95b886e80beab65dd376627771de745
pyvista/pyvista-docs-dev-ja <pyvista/pyvista-docs-dev-ja>
2023-10-06 05:58:16 +0000

    [ci skip] 17450880933 https://github.com/pyvista/pyvista-docs-dev-ja/actions/runs/6426540172/job/17450880933

--- api/utilities/_autosummary/pyvista-Cylinder-1.py
+++ api/utilities/_autosummary/pyvista-Cylinder-1.py
@@ -1,6 +1,16 @@
 import pyvista
-import numpy as np
 cylinder = pyvista.Cylinder(
     center=[1, 2, 3], direction=[1, 1, 1], radius=1, height=2
 )
 cylinder.plot(show_edges=True, line_width=5, cpos='xy')
+#
+pl = pyvista.Plotter()
+_ = pl.add_mesh(
+    pyvista.Cylinder(
+        center=[1, 2, 3], direction=[1, 1, 1], radius=1, height=2
+    ),
+    show_edges=True,
+    line_width=5,
+)
+pl.camera_position = "xy"
+pl.show()
--- api/utilities/_autosummary/pyvista-CylinderSource-1.py
+++ api/utilities/_autosummary/pyvista-CylinderSource-1.py
@@ -3,3 +3,20 @@
 import pyvista
 source = pyvista.CylinderSource()
 source.output.plot(show_edges=True, line_width=5)
+#
+# Display a 3D plot of a default :class:`CylinderSource`.
+#
+import pyvista
+pl = pyvista.Plotter()
+_ = pl.add_mesh(
+    pyvista.CylinderSource(), show_edges=True, line_width=5
+)
+pl.show()
+#
+# Visualize the output of :class:`CylinderSource` in a 3D plot.
+#
+pl = pyvista.Plotter()
+_ = pl.add_mesh(
+    pyvista.CylinderSource().output, show_edges=True, line_width=5
+)
+pl.show()
