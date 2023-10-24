0d656ed888e3eeda0d0626f7deb7069bc1184bc2
pyvista/pyvista-docs-dev-ja <pyvista/pyvista-docs-dev-ja>
2023-10-24 06:42:13 +0000

    [ci skip] 17985112201 https://github.com/pyvista/pyvista-docs-dev-ja/actions/runs/6621316879/job/17985112201

---
+++ api/plotting/_autosummary/pyvista-CornerAnnotation-1.py
@@ -0,0 +1,5 @@
+# Create text annotation in four corners.
+#
+from pyvista import CornerAnnotation
+text = CornerAnnotation(0, 'text')
+prop = text.prop
--- api/plotting/_autosummary/pyvista-Plotter-open_movie-1.py
+++ api/plotting/_autosummary/pyvista-Plotter-open_movie-1.py
@@ -1,5 +1,5 @@
 # Open a MP4 movie and set the quality to maximum.
 #
 import pyvista as pv
-pl = pv.Plotter
+pl = pv.Plotter()
 pl.open_movie('movie.mp4', quality=10)  # doctest:+SKIP
