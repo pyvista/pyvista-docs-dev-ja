7e4dae5516d77912630076657b3fe25f10ef690f
pyvista/pyvista-docs-dev-ja <pyvista/pyvista-docs-dev-ja>
2023-11-02 05:51:44 +0000

    [ci skip] 18284955990 https://github.com/pyvista/pyvista-docs-dev-ja/actions/runs/6727319991/job/18284955990

--- api/core/pointsets-5.py
+++ api/core/pointsets-5.py
@@ -1,7 +1,6 @@
 # Animate plot as a wire-frame
 plotter = pv.Plotter(window_size=(800, 600))
-plotter.add_mesh(grid, scalars=d[:, 1],
-                 scalar_bar_args={'title': 'Y Displacement'},
+plotter.add_mesh(grid, scalars='Y Displacement',
                  show_edges=True,
                  rng=[-d.max(), d.max()], interpolate_before_map=True,
                  style='wireframe')
@@ -10,9 +9,8 @@ plotter.camera_position = cpos
 
 plotter.open_gif('beam_wireframe.gif')
 for phase in np.linspace(0, 2*np.pi, 20):
-    plotter.update_coordinates(grid.points + d*np.cos(phase), render=False)
-    plotter.update_scalars(d[:, 1]*np.cos(phase), render=False)
-    plotter.render()
+    grid.points = pts + d * np.cos(phase)
+    grid['Y Displacement'] = d[:, 1] * np.cos(phase)
     plotter.write_frame()
 
 # close the plotter when complete
