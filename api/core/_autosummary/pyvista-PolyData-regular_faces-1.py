87a36c9a0968aff9082fddcd9a77072d2dcc2614
pyvista/pyvista-docs-dev-ja <pyvista/pyvista-docs-dev-ja>
2023-11-16 03:55:23 +0000

    [ci skip] 18729293021 https://github.com/pyvista/pyvista-docs-dev-ja/actions/runs/6885322888/job/18729293021

--- api/core/_autosummary/pyvista-PolyData-regular_faces-1.py
+++ api/core/_autosummary/pyvista-PolyData-regular_faces-1.py
@@ -1,10 +1,11 @@
-# Get the face array of a tetrahedron as a 4x3 array
+# Get the regular face array of a plane with 2x2 arrangement of cells
+# as a 4x4 array.
 #
 import pyvista as pv
-tetra = pv.Tetrahedron()
-tetra.regular_faces
+plane = pv.Plane(i_resolution=2, j_resolution=2)
+plane.regular_faces
 # Expected:
-## array([[0, 1, 2],
-##        [1, 3, 2],
-##        [0, 2, 3],
-##        [0, 3, 1]])
+## array([[0, 1, 4, 3],
+##        [1, 2, 5, 4],
+##        [3, 4, 7, 6],
+##        [4, 5, 8, 7]])
